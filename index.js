632f946fc5299a93a2a216980216663d12107278
tomacht <thomas@flexio.fr>
2019-10-03 16:43:53 +0200

    add import

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import {globalFlexioImport} from '@flexio-oss/global-import-registry'
 import {deepKeyAssigner} from '@flexio-oss/js-generator-helpers'
 import {RouteWithParams} from './src/js/Route/RouteWithParams'
 
-export {RouterBuilder} from './src/js/RouterBuilder'
-export {TypeCheck} from './src/js/TypeCheck'
-export {RouteException} from './src/js/Route/RouteException'
-
 /**
  * @property {RouteWithParams} globalFlexioImport.io.js_router.types.RouteWithParams
  */
 deepKeyAssigner(globalFlexioImport, 'io.flexio.js_router.types.RouteWithParams', RouteWithParams)
+
+export {RouterBuilder} from './src/js/RouterBuilder'
+export {TypeCheck} from './src/js/TypeCheck'
+export {RouteException} from './src/js/Route/RouteException'
